d04ce788a512f188eb58478ebee48df645838bf0
bokwoon <bokwoon.c@gmail.com>
2024-02-27 04:34:01 +0800

    checkpoint

--- notebrew.go
+++ notebrew.go
@@ -6,7 +6,9 @@ import (
 	"compress/gzip"
 	"context"
 	"crypto/rand"
+	"crypto/sha256"
 	"database/sql"
+	"embed"
 	"encoding/base32"
 	"encoding/base64"
 	"encoding/binary"
@@ -17,6 +19,7 @@ import (
 	"hash"
 	"html/template"
 	"io"
+	"io/fs"
 	"log/slog"
 	"mime"
 	"net/http"
@@ -34,6 +37,57 @@ import (
 	"github.com/yuin/goldmark/text"
 	"golang.org/x/crypto/blake2b"
 )
+
+//go:embed embed static
+var embedFS embed.FS
+
+var RuntimeFS fs.FS = embedFS
+
+var (
+	stylesCSS                    string
+	baselineJS                   string
+	contentSecurityPolicy        string
+	contentSecurityPolicyCaptcha string
+)
+
+func init() {
+	// styles.css
+	b, err := fs.ReadFile(embedFS, "static/styles.css")
+	if err != nil {
+		return
+	}
+	// We don't need to calculate stylesCSSHash because we are already allowing
+	// style-src 'unsafe-inline' in our Content-Security-Policy.
+	stylesCSS = string(b)
+	// baseline.js
+	b, err = fs.ReadFile(embedFS, "static/baseline.js")
+	if err != nil {
+		return
+	}
+	b = bytes.ReplaceAll(b, []byte("\r\n"), []byte("\n"))
+	hash := sha256.Sum256(b)
+	baselineJS = string(b)
+	baselineJSHash := "'sha256-" + base64.StdEncoding.EncodeToString(hash[:]) + "'"
+	// contentSecurityPolicy
+	contentSecurityPolicy = "default-src 'none';" +
+		" script-src 'self' 'unsafe-hashes' " + baselineJSHash + ";" +
+		" connect-src 'self';" +
+		" img-src 'self' data:;" +
+		" style-src 'self' 'unsafe-inline';" +
+		" base-uri 'self';" +
+		" form-action 'self';" +
+		" manifest-src 'self';"
+	// contentSecurityPolicyCaptcha
+	contentSecurityPolicyCaptcha = "default-src 'none';" +
+		" script-src 'self' 'unsafe-hashes' " + baselineJSHash + " https://hcaptcha.com https://*.hcaptcha.com;" +
+		" connect-src 'self' https://hcaptcha.com https://*.hcaptcha.com;" +
+		" img-src 'self' data:;" +
+		" style-src 'self' 'unsafe-inline' https://hcaptcha.com https://*.hcaptcha.com;" +
+		" base-uri 'self';" +
+		" form-action 'self';" +
+		" manifest-src 'self';" +
+		" frame-src https://hcaptcha.com https://*.hcaptcha.com;"
+}
 
 // Notebrew represents a notebrew instance.
 type Notebrew struct {
