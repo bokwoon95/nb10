0428f5127da55ea7f5634fc521fb4e4e31bf9614
bokwoon <bokwoon.c@gmail.com>
2024-02-28 23:33:53 +0800

    checkpoint

--- site_json.go
+++ site_json.go
@@ -1,14 +1,22 @@
 package nb10
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"html/template"
+	"io"
 	"io/fs"
+	"mime"
 	"net/http"
-	"net/url"
 	"path"
 	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/bokwoon95/nb10/sq"
+	"golang.org/x/sync/errgroup"
 )
 
 var chromaStyles = map[string]bool{
@@ -42,8 +50,10 @@ func (nbrew *Notebrew) siteJSON(w http.ResponseWriter, r *http.Request, username
 		NavigationLinks []NavigationLink `json:"navigationLinks"`
 	}
 	type Response struct {
-		Error           string           `json:"error,omitempty"`
-		FormErrors      url.Values       `json:"formErrors,omitempty"`
+		PostRedirectGet map[string]any   `json:"postRedirectGet,omitempty"`
+		Count           int              `json:"count"`
+		TimeTaken       string           `json:"timeTaken"`
+		TemplateError   TemplateError    `json:"templateError,omitempty"`
 		ContentSite     string           `json:"contentSite"`
 		Username        NullString       `json:"username"`
 		SitePrefix      string           `json:"sitePrefix"`
@@ -53,6 +63,27 @@ func (nbrew *Notebrew) siteJSON(w http.ResponseWriter, r *http.Request, username
 		CodeStyle       string           `json:"codeStyle"`
 		Description     string           `json:"description"`
 		NavigationLinks []NavigationLink `json:"navigationLinks"`
+	}
+	normalizeRequest := func(request Request) Request {
+		if request.Title == "" {
+			request.Title = "My Blog"
+		}
+		if request.Emoji == "" {
+			request.Emoji = "☕"
+		}
+		if !chromaStyles[request.CodeStyle] {
+			request.CodeStyle = "onedark"
+		}
+		if request.Description == "" {
+			request.Description = "# Hello World!\n\nWelcome to my blog."
+		}
+		if len(request.NavigationLinks) == 0 {
+			request.NavigationLinks = []NavigationLink{
+				{Name: "home", URL: "/"},
+				{Name: "posts", URL: "/posts/"},
+			}
+		}
+		return request
 	}
 
 	switch r.Method {
@@ -99,10 +130,6 @@ func (nbrew *Notebrew) siteJSON(w http.ResponseWriter, r *http.Request, username
 		response.ContentSite = nbrew.contentSite(sitePrefix)
 		response.Username = NullString{String: username, Valid: nbrew.DB != nil}
 		response.SitePrefix = sitePrefix
-		if response.Error != "" {
-			writeResponse(w, r, response)
-			return
-		}
 		b, err := fs.ReadFile(nbrew.FS.WithContext(r.Context()), path.Join(sitePrefix, "site.json"))
 		if err != nil && !errors.Is(err, fs.ErrNotExist) {
 			getLogger(r.Context()).Error(err.Error())
@@ -118,26 +145,372 @@ func (nbrew *Notebrew) siteJSON(w http.ResponseWriter, r *http.Request, username
 				return
 			}
 		}
+		request = normalizeRequest(request)
 		response.Title = request.Title
-		if response.Title == "" {
-			response.Title = "My Blog"
-		}
 		response.Emoji = request.Emoji
-		if response.Emoji == "" {
-			response.Emoji = "☕"
-		}
 		response.Favicon = request.Favicon
 		response.CodeStyle = request.CodeStyle
-		if !chromaStyles[response.CodeStyle] {
-			response.CodeStyle = "onedark"
-		}
 		response.Description = request.Description
-		if response.Description == "" {
-			response.Description = "# Hello World!\n\nWelcome to my blog."
-		}
 		response.NavigationLinks = request.NavigationLinks
 		writeResponse(w, r, response)
 	case "POST":
+		writeResponse := func(w http.ResponseWriter, r *http.Request, response Response) {
+			if r.Form.Has("api") {
+				w.Header().Set("Content-Type", "application/json")
+				encoder := json.NewEncoder(w)
+				encoder.SetEscapeHTML(false)
+				err := encoder.Encode(&response)
+				if err != nil {
+					getLogger(r.Context()).Error(err.Error())
+				}
+				return
+			}
+			err := nbrew.setSession(w, r, "flash", map[string]any{
+				"postRedirectGet": map[string]any{
+					"from": "site.json",
+				},
+				"count":         response.Count,
+				"timeTaken":     response.TimeTaken,
+				"templateError": response.TemplateError,
+			})
+			if err != nil {
+				getLogger(r.Context()).Error(err.Error())
+				internalServerError(w, r, err)
+				return
+			}
+			http.Redirect(w, r, "/"+path.Join("files", sitePrefix, "site.json"), http.StatusFound)
+		}
+
+		var request Request
+		r.Body = http.MaxBytesReader(w, r.Body, 1<<20 /* 1 MB */)
+		contentType, _, _ := mime.ParseMediaType(r.Header.Get("Content-Type"))
+		switch contentType {
+		case "application/json":
+			err := json.NewDecoder(r.Body).Decode(&request)
+			if err != nil {
+				badRequest(w, r, err)
+				return
+			}
+		case "application/x-www-form-urlencoded", "multipart/form-data":
+			if contentType == "multipart/form-data" {
+				err := r.ParseMultipartForm(1 << 20 /* 1 MB */)
+				if err != nil {
+					badRequest(w, r, err)
+					return
+				}
+			} else {
+				err := r.ParseForm()
+				if err != nil {
+					badRequest(w, r, err)
+					return
+				}
+			}
+			request.Title = r.Form.Get("title")
+			request.Emoji = r.Form.Get("emoji")
+			request.Favicon = r.Form.Get("favicon")
+			request.CodeStyle = r.Form.Get("codeStyle")
+			request.Description = r.Form.Get("description")
+			navigationLinkNames := r.Form["navigationLinkName"]
+			navigationLinkURLs := r.Form["navigationLinkURL"]
+			for i := range navigationLinkNames {
+				if i >= len(navigationLinkURLs) {
+					break
+				}
+				request.NavigationLinks = append(request.NavigationLinks, NavigationLink{
+					Name: navigationLinkNames[i],
+					URL:  template.URL(navigationLinkURLs[i]),
+				})
+			}
+		default:
+			unsupportedContentType(w, r)
+			return
+		}
+
+		request = normalizeRequest(request)
+		b, err := json.MarshalIndent(&request, "", "  ")
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+			internalServerError(w, r, err)
+			return
+		}
+		writer, err := nbrew.FS.WithContext(r.Context()).OpenWriter(path.Join(sitePrefix, "site.json"), 0644)
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+			internalServerError(w, r, err)
+			return
+		}
+		defer writer.Close()
+		_, err = io.Copy(writer, bytes.NewReader(b))
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+			internalServerError(w, r, err)
+			return
+		}
+
+		siteGen, err := NewSiteGenerator(r.Context(), nbrew.FS, sitePrefix, nbrew.ContentDomain, nbrew.ImgDomain)
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+			internalServerError(w, r, err)
+			return
+		}
+		if remoteFS, ok := nbrew.FS.(*RemoteFS); ok {
+			type File struct {
+				FilePath string
+				IsDir    bool
+				Text     string
+			}
+			var response Response
+			var count atomic.Int64
+			startedAt := time.Now()
+			group, groupctx := errgroup.WithContext(r.Context())
+			group.Go(func() error {
+				cursor, err := sq.FetchCursor(groupctx, remoteFS.DB, sq.Query{
+					Dialect: remoteFS.Dialect,
+					Format: "SELECT {*}" +
+						" FROM files" +
+						" WHERE file_path LIKE {pattern} ESCAPE '\\'" +
+						" AND (NOT is_dir AND file_path LIKE '%.html')",
+					Values: []any{
+						sq.StringParam("pattern", strings.NewReplacer("%", "\\%", "_", "\\_").Replace(path.Join(sitePrefix, "pages"))+"/%"),
+					},
+				}, func(row *sq.Row) File {
+					return File{
+						FilePath: row.String("file_path"),
+						Text:     row.String("text"),
+					}
+				})
+				if err != nil {
+					return err
+				}
+				defer cursor.Close()
+				subgroup, subctx := errgroup.WithContext(groupctx)
+				for cursor.Next() {
+					file, err := cursor.Result()
+					if err != nil {
+						return err
+					}
+					subgroup.Go(func() error {
+						if sitePrefix != "" {
+							_, file.FilePath, _ = strings.Cut(file.FilePath, "/")
+						}
+						count.Add(1)
+						return siteGen.GeneratePage(subctx, file.FilePath, file.Text)
+					})
+				}
+				err = cursor.Close()
+				if err != nil {
+					return err
+				}
+				err = subgroup.Wait()
+				if err != nil {
+					return err
+				}
+				return nil
+			})
+			group.Go(func() error {
+				postTemplates := make(map[string]*template.Template)
+				postTemplatesMu := sync.Mutex{}
+				cursor, err := sq.FetchCursor(groupctx, remoteFS.DB, sq.Query{
+					Dialect: remoteFS.Dialect,
+					Format: "SELECT {*}" +
+						" FROM files" +
+						" WHERE (file_path = {posts} OR file_path LIKE {pattern} ESCAPE '\\')" +
+						" AND (is_dir OR file_path LIKE '%.md')",
+					Values: []any{
+						sq.StringParam("posts", path.Join(sitePrefix, "posts")),
+						sq.StringParam("pattern", strings.NewReplacer("%", "\\%", "_", "\\_").Replace(path.Join(sitePrefix, "posts"))+"/%"),
+					},
+				}, func(row *sq.Row) File {
+					return File{
+						FilePath: row.String("file_path"),
+						IsDir:    row.Bool("is_dir"),
+						Text:     row.String("text"),
+					}
+				})
+				if err != nil {
+					return err
+				}
+				defer cursor.Close()
+				subgroup, subctx := errgroup.WithContext(groupctx)
+				for cursor.Next() {
+					file, err := cursor.Result()
+					if err != nil {
+						return err
+					}
+					subgroup.Go(func() error {
+						if sitePrefix != "" {
+							file.FilePath = strings.TrimPrefix(file.FilePath, sitePrefix+"/")
+						}
+						if !file.IsDir {
+							count.Add(1)
+							postTemplate, err := siteGen.PostTemplate(groupctx)
+							if err != nil {
+								return err
+							}
+							return siteGen.GeneratePost(subctx, file.FilePath, file.Text, postTemplate)
+						}
+						_, category, _ := strings.Cut(file.FilePath, "/")
+						if strings.Contains(category, "/") {
+							return nil
+						}
+						postListTemplate, err := siteGen.PostListTemplate(subctx, category)
+						if err != nil {
+							return err
+						}
+						n, err := siteGen.GeneratePostList(subctx, category, postListTemplate)
+						count.Add(int64(n))
+						if err != nil {
+							return err
+						}
+						return nil
+					})
+				}
+				err = cursor.Close()
+				if err != nil {
+					return err
+				}
+				err = subgroup.Wait()
+				if err != nil {
+					return err
+				}
+				return nil
+			})
+			err = group.Wait()
+			if err != nil {
+				if !errors.As(err, &response.TemplateError) {
+					getLogger(r.Context()).Error(err.Error())
+					internalServerError(w, r, err)
+					return
+				}
+			}
+			response.Count = int(count.Load())
+			response.TimeTaken = time.Since(startedAt).String()
+			writeResponse(w, r, response)
+			return
+		}
+		var response Response
+		var count atomic.Int64
+		startedAt := time.Now()
+		group, groupctx := errgroup.WithContext(r.Context())
+		group.Go(func() error {
+			subgroup, subctx := errgroup.WithContext(groupctx)
+			root := path.Join(sitePrefix, "pages")
+			err := fs.WalkDir(nbrew.FS.WithContext(groupctx), root, func(filePath string, dirEntry fs.DirEntry, err error) error {
+				if err != nil {
+					return err
+				}
+				if filePath == root {
+					return nil
+				}
+				subgroup.Go(func() error {
+					file, err := nbrew.FS.WithContext(subctx).Open(filePath)
+					if err != nil {
+						return err
+					}
+					fileInfo, err := file.Stat()
+					if err != nil {
+						return err
+					}
+					if fileInfo.IsDir() || !strings.HasSuffix(filePath, ".html") {
+						return nil
+					}
+					var b strings.Builder
+					b.Grow(int(fileInfo.Size()))
+					_, err = io.Copy(&b, file)
+					if err != nil {
+						return err
+					}
+					if sitePrefix != "" {
+						filePath = strings.TrimPrefix(filePath, sitePrefix+"/")
+					}
+					count.Add(1)
+					return siteGen.GeneratePage(subctx, filePath, b.String())
+				})
+				return nil
+			})
+			err = subgroup.Wait()
+			if err != nil {
+				return err
+			}
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+		group.Go(func() error {
+			postTemplate, err := siteGen.PostTemplate(groupctx)
+			if err != nil {
+				return err
+			}
+			subgroup, subctx := errgroup.WithContext(groupctx)
+			root := path.Join(sitePrefix, "posts")
+			err = fs.WalkDir(nbrew.FS.WithContext(groupctx), root, func(filePath string, dirEntry fs.DirEntry, err error) error {
+				if err != nil {
+					return err
+				}
+				subgroup.Go(func() error {
+					if !dirEntry.IsDir() {
+						if !strings.HasSuffix(filePath, ".md") {
+							return nil
+						}
+						file, err := nbrew.FS.WithContext(subctx).Open(filePath)
+						if err != nil {
+							return err
+						}
+						fileInfo, err := file.Stat()
+						if err != nil {
+							return err
+						}
+						var b strings.Builder
+						b.Grow(int(fileInfo.Size()))
+						_, err = io.Copy(&b, file)
+						if err != nil {
+							return err
+						}
+						if sitePrefix != "" {
+							filePath = strings.TrimPrefix(filePath, sitePrefix+"/")
+						}
+						count.Add(1)
+						return siteGen.GeneratePost(subctx, filePath, b.String(), postTemplate)
+					}
+					if sitePrefix != "" {
+						filePath = strings.TrimPrefix(filePath, sitePrefix+"/")
+					}
+					_, category, _ := strings.Cut(filePath, "/")
+					if strings.Contains(category, "/") {
+						return nil
+					}
+					postListTemplate, err := siteGen.PostListTemplate(subctx, category)
+					if err != nil {
+						return err
+					}
+					n, err := siteGen.GeneratePostList(subctx, category, postListTemplate)
+					count.Add(int64(n))
+					if err != nil {
+						return err
+					}
+					return nil
+				})
+				return nil
+			})
+			err = subgroup.Wait()
+			if err != nil {
+				return err
+			}
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+		err = group.Wait()
+		response.Count = int(count.Load())
+		response.TimeTaken = time.Since(startedAt).String()
+		if err != nil && !errors.As(err, &response.TemplateError) {
+			getLogger(r.Context()).Error(err.Error())
+			internalServerError(w, r, err)
+			return
+		}
+		writeResponse(w, r, response)
 	default:
 		methodNotAllowed(w, r)
 	}
