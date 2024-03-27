8a501c226fdaf885420155f7c13b155058b143b4
bokwoon <bokwoon.c@gmail.com>
2024-03-28 03:44:50 +0800

    checkpoint

--- database_fs.go
+++ database_fs.go
@@ -1508,7 +1508,7 @@ func (storage *S3ObjectStorage) Put(ctx context.Context, key string, reader io.R
 	}
 	var parts []types.CompletedPart
 	var partNumber int32
-	var buf [5 << 20]byte
+	var buf [5 << 20]byte // TODO: Is this stack allocated? Would be expensive if not.
 	done := false
 	for !done {
 		n, err := io.ReadFull(reader, buf[:])
