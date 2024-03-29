20f07a4cb4c82fd425df7939f577f0340ebc19ce
bokwoon <bokwoon.c@gmail.com>
2024-03-29 21:53:50 +0800

    checkpoint

--- pin.go
+++ pin.go
@@ -1,17 +1,11 @@
 package nb10
 
 import (
-	"encoding/json"
-	"errors"
-	"html/template"
 	"io/fs"
 	"net/http"
 	"path"
-	"path/filepath"
 	"strings"
 	"time"
-
-	"golang.org/x/sync/errgroup"
 )
 
 func (nbrew *Notebrew) pin(w http.ResponseWriter, r *http.Request, user User, sitePrefix string) {
@@ -39,9 +33,8 @@ func (nbrew *Notebrew) pin(w http.ResponseWriter, r *http.Request, user User, si
 		Files          []File `json:"files"`
 		Error          string `json:"status"`
 	}
-
-	if nbrew.DB == nil {
-		nbrew.notFound(w, r)
+	if r.Method != "POST" {
+		nbrew.methodNotAllowed(w, r)
 		return
 	}
 
@@ -59,115 +52,5 @@ func (nbrew *Notebrew) pin(w http.ResponseWriter, r *http.Request, user User, si
 		}
 		return false
 	}
-
-	switch r.Method {
-	case "GET":
-		writeResponse := func(w http.ResponseWriter, r *http.Request, response Response) {
-			if r.Form.Has("api") {
-				w.Header().Set("Content-Type", "application/json")
-				encoder := json.NewEncoder(w)
-				encoder.SetEscapeHTML(false)
-				err := encoder.Encode(&response)
-				if err != nil {
-					getLogger(r.Context()).Error(err.Error())
-				}
-				return
-			}
-			referer := getReferer(r)
-			funcMap := map[string]any{
-				"join":       path.Join,
-				"ext":        path.Ext,
-				"hasPrefix":  strings.HasPrefix,
-				"trimPrefix": strings.TrimPrefix,
-				"stylesCSS":  func() template.CSS { return template.CSS(StylesCSS) },
-				"baselineJS": func() template.JS { return template.JS(BaselineJS) },
-				"referer":    func() string { return referer },
-			}
-			tmpl, err := template.New("pin.html").Funcs(funcMap).ParseFS(RuntimeFS, "embed/pin.html")
-			if err != nil {
-				getLogger(r.Context()).Error(err.Error())
-				nbrew.internalServerError(w, r, err)
-				return
-			}
-			w.Header().Set("Content-Security-Policy", nbrew.ContentSecurityPolicy)
-			nbrew.executeTemplate(w, r, tmpl, &response)
-		}
-
-		var response Response
-		_, err := nbrew.getSession(r, "flash", &response)
-		if err != nil {
-			getLogger(r.Context()).Error(err.Error())
-		}
-		nbrew.clearSession(w, r, "flash")
-		response.ContentBaseURL = nbrew.contentBaseURL(sitePrefix)
-		response.ImgDomain = nbrew.ImgDomain
-		_, response.IsDatabaseFS = nbrew.FS.(*DatabaseFS)
-		response.UserID = user.UserID
-		response.Username = user.Username
-		response.SitePrefix = sitePrefix
-		response.Unpin = r.Form.Has("unpin")
-		response.Parent = path.Clean(strings.Trim(r.Form.Get("parent"), "/"))
-		if response.Error != "" {
-			writeResponse(w, r, response)
-			return
-		}
-		if !isValidParent(response.Parent) {
-			response.Error = "InvalidParent"
-			writeResponse(w, r, response)
-			return
-		}
-		seen := make(map[string]bool)
-		group, groupctx := errgroup.WithContext(r.Context())
-		names := r.Form["name"]
-		response.Files = make([]File, len(names))
-		for i, name := range names {
-			i, name := i, filepath.ToSlash(name)
-			if strings.Contains(name, "/") {
-				continue
-			}
-			if seen[name] {
-				continue
-			}
-			seen[name] = true
-			group.Go(func() error {
-				fileInfo, err := fs.Stat(nbrew.FS.WithContext(groupctx), path.Join(sitePrefix, response.Parent, name))
-				if err != nil {
-					if errors.Is(err, fs.ErrNotExist) {
-						return nil
-					}
-					return err
-				}
-				file := File{
-					Name:    fileInfo.Name(),
-					IsDir:   fileInfo.IsDir(),
-					Size:    fileInfo.Size(),
-					ModTime: fileInfo.ModTime(),
-				}
-				if fileInfo, ok := fileInfo.(*DatabaseFileInfo); ok {
-					file.FileID = fileInfo.FileID
-				}
-				response.Files[i] = file
-				return nil
-			})
-		}
-		err = group.Wait()
-		if err != nil {
-			getLogger(r.Context()).Error(err.Error())
-			nbrew.internalServerError(w, r, err)
-			return
-		}
-		n := 0
-		for _, file := range response.Files {
-			if file.Name == "" {
-				continue
-			}
-			response.Files[n] = file
-			n++
-		}
-		response.Files = response.Files[:n]
-		writeResponse(w, r, response)
-	case "POST":
-	default:
-		nbrew.methodNotAllowed(w, r)
-	}
+	_ = isValidParent
 }
