528151c1c0ee055e171b179084bd6e7e63414aac
bokwoon <bokwoon.c@gmail.com>
2024-04-09 11:46:29 +0800

    checkpoint

--- static/insert.js
+++ static/insert.js
@@ -1,3 +1,14 @@
+const dataEditor = document.querySelector("[data-editor]");
+if (dataEditor) {
+  const config = new Map();
+  let obj = JSON.parse(dataEditor.getAttribute("data-editor") || "{}");
+  for (const [key, value] of Object.entries(obj)) {
+    config.set(key, value);
+  }
+  const textarea = dataEditor.querySelector("textarea");
+  if (!textarea) {
+  }
+}
 for (const dataInsert of document.querySelectorAll("[data-insert]")) {
   dataInsert.addEventListener("click", function() {
   });
