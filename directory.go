6d72ff46bde07688e760b8ba210dd4b5164e2d07
bokwoon <bokwoon.c@gmail.com>
2024-04-12 01:58:11 +0800

    checkpoint

--- directory.go
+++ directory.go
@@ -375,7 +375,7 @@ func (nbrew *Notebrew) directory(w http.ResponseWriter, r *http.Request, user Us
 
 	response.Limit, _ = strconv.Atoi(r.Form.Get("limit"))
 	if response.Limit <= 0 {
-		response.Limit = 1000
+		response.Limit = 300
 	}
 	scheme := "https"
 	if r.TLS == nil {
