6032dd0faa8232adc5d75cece60bcf7b18b47a43
bokwoon <bokwoon.c@gmail.com>
2024-04-14 23:09:05 +0800

    checkpoint

--- export.go
+++ export.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"path"
 	"strings"
+	"time"
 )
 
 func (nbrew *Notebrew) export(w http.ResponseWriter, r *http.Request, user User, sitePrefix string) {
@@ -18,6 +19,16 @@ func (nbrew *Notebrew) export(w http.ResponseWriter, r *http.Request, user User,
 		return
 	}
 	parent := path.Clean(strings.Trim(r.Form.Get("parent"), "/"))
+	if parent == "." {
+		if databaseFS, ok := nbrew.FS.(*DatabaseFS); ok {
+			_ = databaseFS
+			if sitePrefix == "" {
+				return
+			}
+		} else {
+		}
+		return
+	}
 	head, _, _ := strings.Cut(parent, "/")
 	switch head {
 	case "notes", "pages", "posts", "output":
@@ -40,14 +51,48 @@ func (nbrew *Notebrew) export(w http.ResponseWriter, r *http.Request, user User,
 		return
 	}
 	names := r.Form["name"]
+	gzipWriter := gzipWriterPool.Get().(*gzip.Writer)
+	gzipWriter.Reset(w)
+	defer func() {
+		gzipWriter.Reset(io.Discard)
+		gzipWriterPool.Put(gzipWriter)
+	}()
+	tarWriter := tar.NewWriter(gzipWriter)
+	defer tarWriter.Close()
 	if len(names) == 0 {
-		gzipWriter := gzipWriterPool.Get().(*gzip.Writer)
-		gzipWriter.Reset(w)
-		defer func() {
-			gzipWriter.Reset(io.Discard)
-			gzipWriterPool.Put(gzipWriter)
-		}()
-		tarWriter := tar.NewWriter(gzipWriter)
-		defer tarWriter.Close()
+		var fileName string
+		if sitePrefix == "" {
+			fileName = "files-" + time.Now().UTC().Format("20060102150405") + ".tgz"
+		} else {
+			fileName = "files-" + strings.TrimPrefix(sitePrefix, "@") + "-" + time.Now().UTC().Format("20060102150405") + ".tgz"
+		}
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Header().Set("Content-Disposition", `attachment; filename="`+fileName+`"`)
+		if databaseFS, ok := nbrew.FS.(*DatabaseFS); ok {
+			_ = databaseFS
+		} else {
+			subFS, err := fs.Sub(nbrew.FS.WithContext(r.Context()), parent)
+			if err != nil {
+				getLogger(r.Context()).Error(err.Error())
+				nbrew.internalServerError(w, r, err)
+				return
+			}
+			err = tarWriter.AddFS(subFS)
+			if err != nil {
+				getLogger(r.Context()).Error(err.Error())
+				return
+			}
+			err = tarWriter.Close()
+			if err != nil {
+				getLogger(r.Context()).Error(err.Error())
+				return
+			}
+			err = gzipWriter.Close()
+			if err != nil {
+				getLogger(r.Context()).Error(err.Error())
+				return
+			}
+			return
+		}
 	}
 }
