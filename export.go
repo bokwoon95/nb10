586ba5a326bb48dae1bda69ffa32b77c2426f225
bokwoon <bokwoon.c@gmail.com>
2024-04-21 19:16:56 +0800

    checkpoint

--- export.go
+++ export.go
@@ -45,9 +45,11 @@ func (nbrew *Notebrew) export(w http.ResponseWriter, r *http.Request, user User,
 	gzipWriter := gzipWriterPool.Get().(*gzip.Writer)
 	// gzipWriter.Reset(w)
 	defer func() {
-		gzipWriter.Close()
-		gzipWriter.Reset(io.Discard)
-		gzipWriterPool.Put(gzipWriter)
+		if gzipWriter != nil {
+			gzipWriter.Close()
+			gzipWriter.Reset(io.Discard)
+			gzipWriterPool.Put(gzipWriter)
+		}
 	}()
 	tarWriter := tar.NewWriter(w)
 	defer tarWriter.Close()
