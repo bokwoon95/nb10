6764f839f4a070a4eb15c6fc41df39f9330e04a1
bokwoon <bokwoon.c@gmail.com>
2024-06-10 18:31:26 +0800

    checkpoint

--- directory.go
+++ directory.go
@@ -1095,3 +1095,285 @@ func (nbrew *Notebrew) directory(w http.ResponseWriter, r *http.Request, user Us
 	writeResponse(w, r, response)
 	return
 }
+
+func (nbrew *Notebrew) directoryV2(w http.ResponseWriter, r *http.Request, user User, sitePrefix, filePath string, fileInfo fs.FileInfo) {
+	type File struct {
+		FileID       ID        `json:"fileID"`
+		Parent       string    `json:"parent"`
+		Name         string    `json:"name"`
+		IsDir        bool      `json:"isDir"`
+		ModTime      time.Time `json:"modTime"`
+		CreationTime time.Time `json:"creationTime"`
+		Size         int64     `json:"size"`
+	}
+	type Response struct {
+		ContentBaseURL    string            `json:"contentBaseURL"`
+		ImgDomain         string            `json:"imgDomain"`
+		IsDatabaseFS      bool              `json:"isDatabaseFS"`
+		SitePrefix        string            `json:"sitePrefix"`
+		UserID            ID                `json:"userID"`
+		Username          string            `json:"username"`
+		FileID            ID                `json:"fileID"`
+		FilePath          string            `json:"filePath"`
+		IsDir             bool              `json:"isDir"`
+		ModTime           time.Time         `json:"modTime"`
+		CreationTime      time.Time         `json:"creationTime"`
+		PinnedFiles       []File            `json:"pinnedFiles"`
+		Files             []File            `json:"files"`
+		Sort              string            `json:"sort"`
+		Order             string            `json:"order"`
+		From              string            `json:"from"`
+		FromCreated       string            `json:"fromCreated"`
+		FromEdited        string            `json:"fromEdited"`
+		FromTime          string            `json:"fromTime"`
+		Before            string            `json:"before"`
+		BeforeCreated     string            `json:"beforeCreated"`
+		BeforeEdited      string            `json:"beforeEdited"`
+		BeforeTime        string            `json:"beforeTime"`
+		Limit             int               `json:"limit"`
+		PreviousURL       string            `json:"previousURL"`
+		NextURL           string            `json:"nextURL"`
+		RegenerationStats RegenerationStats `json:"regenerationStats"`
+		PostRedirectGet   map[string]any    `json:"postRedirectGet"`
+	}
+	writeResponse := func(w http.ResponseWriter, r *http.Request, response Response) {
+		if response.PinnedFiles == nil {
+			response.PinnedFiles = []File{}
+		}
+		if response.Files == nil {
+			response.Files = []File{}
+		}
+		if r.Form.Has("api") {
+			w.Header().Set("Content-Type", "application/json")
+			encoder := json.NewEncoder(w)
+			encoder.SetIndent("", "  ")
+			encoder.SetEscapeHTML(false)
+			err := encoder.Encode(&response)
+			if err != nil {
+				getLogger(r.Context()).Error(err.Error())
+			}
+			return
+		}
+		referer := nbrew.getReferer(r)
+		clipboard := make(url.Values)
+		isInClipboard := make(map[string]bool)
+		cookie, _ := r.Cookie("clipboard")
+		if cookie != nil {
+			values, err := url.ParseQuery(cookie.Value)
+			if err == nil {
+				sourceSitePrefix := values.Get("sitePrefix")
+				if sourceSitePrefix == sitePrefix {
+					if values.Has("cut") {
+						clipboard.Set("cut", "")
+					}
+					clipboard.Set("sitePrefix", values.Get("sitePrefix"))
+					clipboard.Set("parent", values.Get("parent"))
+					for _, name := range values["name"] {
+						if isInClipboard[name] {
+							continue
+						}
+						clipboard.Add("name", name)
+						isInClipboard[name] = true
+					}
+				}
+			}
+		}
+		funcMap := map[string]any{
+			"join":                  path.Join,
+			"dir":                   path.Dir,
+			"base":                  path.Base,
+			"ext":                   path.Ext,
+			"hasPrefix":             strings.HasPrefix,
+			"hasSuffix":             strings.HasSuffix,
+			"trimPrefix":            strings.TrimPrefix,
+			"trimSuffix":            strings.TrimSuffix,
+			"humanReadableFileSize": humanReadableFileSize,
+			"stylesCSS":             func() template.CSS { return template.CSS(StylesCSS) },
+			"baselineJS":            func() template.JS { return template.JS(BaselineJS) },
+			"referer":               func() string { return referer },
+			"clipboard":             func() url.Values { return clipboard },
+			"safeHTML":              func(s string) template.HTML { return template.HTML(s) },
+			"float64ToInt64":        func(n float64) int64 { return int64(n) },
+			"head": func(s string) string {
+				head, _, _ := strings.Cut(s, "/")
+				return head
+			},
+			"tail": func(s string) string {
+				_, tail, _ := strings.Cut(s, "/")
+				return tail
+			},
+			"generateBreadcrumbLinks": func(sitePrefix, filePath string) template.HTML {
+				var b strings.Builder
+				b.WriteString("<a href='/files/'>files</a>")
+				segments := strings.Split(filePath, "/")
+				if sitePrefix != "" {
+					segments = append([]string{sitePrefix}, segments...)
+				}
+				for i := 0; i < len(segments); i++ {
+					if segments[i] == "" {
+						continue
+					}
+					href := "/files/" + path.Join(segments[:i+1]...) + "/"
+					b.WriteString(" / <a href='" + href + "'>" + segments[i] + "</a>")
+				}
+				b.WriteString(" /")
+				return template.HTML(b.String())
+			},
+			"isInClipboard": func(name string) bool {
+				if sitePrefix != clipboard.Get("sitePrefix") {
+					return false
+				}
+				if response.FilePath != clipboard.Get("parent") {
+					return false
+				}
+				return isInClipboard[name]
+			},
+		}
+		tmpl, err := template.New("directory.html").Funcs(funcMap).ParseFS(RuntimeFS, "embed/directory.html")
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+			nbrew.internalServerError(w, r, err)
+			return
+		}
+		w.Header().Set("Content-Security-Policy", nbrew.ContentSecurityPolicy)
+		nbrew.executeTemplate(w, r, tmpl, &response)
+	}
+	if r.Method != "GET" {
+		nbrew.methodNotAllowed(w, r)
+		return
+	}
+
+	head, _, _ := strings.Cut(filePath, "/")
+	if head != "notes" && head != "pages" && head != "posts" && head != "output" {
+		nbrew.notFound(w, r)
+		return
+	}
+
+	var response Response
+	_, err := nbrew.getSession(r, "flash", &response)
+	if err != nil {
+		getLogger(r.Context()).Error(err.Error())
+	}
+	nbrew.clearSession(w, r, "flash")
+	response.ContentBaseURL = nbrew.contentBaseURL(sitePrefix)
+	response.ImgDomain = nbrew.ImgDomain
+	_, response.IsDatabaseFS = nbrew.FS.(*DatabaseFS)
+	response.SitePrefix = sitePrefix
+	response.UserID = user.UserID
+	response.Username = user.Username
+	if fileInfo, ok := fileInfo.(*DatabaseFileInfo); ok {
+		response.FileID = fileInfo.FileID
+		response.ModTime = fileInfo.ModTime()
+		response.CreationTime = fileInfo.CreationTime
+	} else {
+		var absolutePath string
+		if dirFS, ok := nbrew.FS.(*DirFS); ok {
+			absolutePath = path.Join(dirFS.RootDir, response.SitePrefix, response.FilePath)
+		}
+		response.CreationTime = CreationTime(absolutePath, fileInfo)
+	}
+	response.FilePath = filePath
+	response.IsDir = true
+	var sortCookie, orderCookie *http.Cookie
+	for _, cookie := range r.Cookies() {
+		switch cookie.Name {
+		case "sort":
+			sortCookie = cookie
+		case "order":
+			orderCookie = cookie
+		}
+	}
+	response.Sort = strings.ToLower(strings.TrimSpace(r.Form.Get("sort")))
+	if response.Sort == "" && sortCookie != nil {
+		response.Sort = sortCookie.Value
+	}
+	switch response.Sort {
+	case "name", "edited", "created":
+		break
+	default:
+		if head == "notes" {
+			response.Sort = "edited"
+		} else if head == "posts" {
+			response.Sort = "created"
+		} else {
+			response.Sort = "name"
+		}
+	}
+	response.Order = strings.ToLower(strings.TrimSpace(r.Form.Get("order")))
+	if response.Order == "" && orderCookie != nil {
+		response.Order = orderCookie.Value
+	}
+	switch response.Order {
+	case "asc", "desc":
+		break
+	default:
+		if response.Sort == "created" || response.Sort == "edited" {
+			response.Order = "desc"
+		} else {
+			response.Order = "asc"
+		}
+	}
+	if r.Form.Has("persist") {
+		if r.Form.Has("sort") {
+			isDefaultSort := false
+			if head == "notes" {
+				isDefaultSort = response.Sort == "edited"
+			} else if head == "posts" {
+				isDefaultSort = response.Sort == "created"
+			} else {
+				isDefaultSort = response.Sort == "name"
+			}
+			if isDefaultSort {
+				http.SetCookie(w, &http.Cookie{
+					Path:     r.URL.EscapedPath(),
+					Name:     "sort",
+					Value:    "0",
+					MaxAge:   -1,
+					Secure:   r.TLS != nil,
+					HttpOnly: true,
+					SameSite: http.SameSiteLaxMode,
+				})
+			} else {
+				http.SetCookie(w, &http.Cookie{
+					Path:     r.URL.EscapedPath(),
+					Name:     "sort",
+					Value:    response.Sort,
+					MaxAge:   int((time.Hour * 24 * 365).Seconds()),
+					Secure:   r.TLS != nil,
+					HttpOnly: true,
+					SameSite: http.SameSiteLaxMode,
+				})
+			}
+		}
+		if r.Form.Has("order") {
+			isDefaultOrder := false
+			if response.Sort == "created" || response.Sort == "edited" {
+				isDefaultOrder = response.Order == "desc"
+			} else {
+				isDefaultOrder = response.Order == "asc"
+			}
+			if isDefaultOrder {
+				http.SetCookie(w, &http.Cookie{
+					Path:     r.URL.EscapedPath(),
+					Name:     "order",
+					Value:    "0",
+					MaxAge:   -1,
+					Secure:   r.TLS != nil,
+					HttpOnly: true,
+					SameSite: http.SameSiteLaxMode,
+				})
+			} else {
+				http.SetCookie(w, &http.Cookie{
+					Path:     r.URL.EscapedPath(),
+					Name:     "order",
+					Value:    response.Order,
+					MaxAge:   int((time.Hour * 24 * 365).Seconds()),
+					Secure:   r.TLS != nil,
+					HttpOnly: true,
+					SameSite: http.SameSiteLaxMode,
+				})
+			}
+		}
+	}
+	writeResponse(w, r, response)
+}
