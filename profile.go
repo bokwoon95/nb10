b5f536fc141fca1c50ff31a84760ba9e72e2ab48
bokwoon <bokwoon.c@gmail.com>
2024-07-01 23:59:29 +0800

    checkpoint

--- profile.go
+++ profile.go
@@ -2,12 +2,16 @@ package nb10
 
 import (
 	"encoding/json"
+	"errors"
 	"html/template"
+	"io/fs"
 	"net/http"
 	"path"
 	"strings"
+	"sync/atomic"
 
 	"github.com/bokwoon95/nb10/sq"
+	"golang.org/x/sync/errgroup"
 )
 
 func (nbrew *Notebrew) profile(w http.ResponseWriter, r *http.Request, user User) {
@@ -27,6 +31,10 @@ func (nbrew *Notebrew) profile(w http.ResponseWriter, r *http.Request, user User
 		StorageUsed     int64          `json:"storageUsed"`
 		Sites           []Site         `json:"sites"`
 		PostRedirectGet map[string]any `json:"postRedirectGet"`
+	}
+	if nbrew.DB == nil {
+		nbrew.notFound(w, r)
+		return
 	}
 	if r.Method != "GET" && r.Method != "HEAD" {
 		nbrew.methodNotAllowed(w, r)
@@ -122,10 +130,80 @@ func (nbrew *Notebrew) profile(w http.ResponseWriter, r *http.Request, user User
 	}
 	response.Sites = sites
 	_, response.IsDatabaseFS = nbrew.FS.(*DatabaseFS)
-	if response.IsDatabaseFS {
-		for _, site := range response.Sites {
-			response.StorageUsed += site.StorageUsed
+	if !response.IsDatabaseFS {
+		group, groupctx := errgroup.WithContext(r.Context())
+		for i, site := range response.Sites {
+			i, site := i, site
+			group.Go(func() error {
+				if site.SiteName == "" {
+					var storageUsed atomic.Int64
+					subgroup, subctx := errgroup.WithContext(groupctx)
+					for _, root := range []string{"notes", "pages", "posts", "output", "import", "export"} {
+						root := root
+						subgroup.Go(func() error {
+							return fs.WalkDir(nbrew.FS.WithContext(subctx), root, func(filePath string, dirEntry fs.DirEntry, err error) error {
+								if err != nil {
+									if filePath == root && errors.Is(err, fs.ErrNotExist) {
+										return nil
+									}
+									return err
+								}
+								if dirEntry.IsDir() {
+									return nil
+								}
+								fileInfo, err := dirEntry.Info()
+								if err != nil {
+									return err
+								}
+								storageUsed.Add(fileInfo.Size())
+								return nil
+							})
+						})
+					}
+					err := subgroup.Wait()
+					if err != nil {
+						return err
+					}
+					response.Sites[i].StorageUsed = storageUsed.Load()
+				} else {
+					var storageUsed int64
+					var sitePrefix string
+					if strings.Contains(site.SiteName, ".") {
+						sitePrefix = site.SiteName
+					} else {
+						sitePrefix = "@" + site.SiteName
+					}
+					err := fs.WalkDir(nbrew.FS.WithContext(groupctx), sitePrefix, func(filePath string, dirEntry fs.DirEntry, err error) error {
+						if err != nil {
+							return err
+						}
+						if dirEntry.IsDir() {
+							return nil
+						}
+						fileInfo, err := dirEntry.Info()
+						if err != nil {
+							return err
+						}
+						storageUsed += fileInfo.Size()
+						return nil
+					})
+					if err != nil {
+						return err
+					}
+					response.Sites[i].StorageUsed = storageUsed
+				}
+				return nil
+			})
 		}
+		err := group.Wait()
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+			nbrew.internalServerError(w, r, err)
+			return
+		}
+	}
+	for _, site := range response.Sites {
+		response.StorageUsed += site.StorageUsed
 	}
 	writeResponse(w, r, response)
 }
