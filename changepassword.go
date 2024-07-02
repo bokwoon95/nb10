ad26b1beed03f36ff5644e0ab1c42460ec9a7a69
bokwoon <bokwoon.c@gmail.com>
2024-07-02 14:08:01 +0800

    checkpoint

---
+++ changepassword.go
@@ -0,0 +1,234 @@
+package nb10
+
+import (
+	"encoding/json"
+	"html/template"
+	"mime"
+	"net/http"
+	"net/url"
+	"path"
+	"strings"
+	"unicode/utf8"
+
+	"github.com/bokwoon95/nb10/sq"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func (nbrew *Notebrew) changepassword(w http.ResponseWriter, r *http.Request, user User) {
+	type Request struct {
+		Password        string `json:"password"`
+		ConfirmPassword string `json:"confirmPassword"`
+	}
+	type Response struct {
+		UserID     ID         `json:"userID"`
+		Username   string     `json:"username"`
+		Error      string     `json:"error"`
+		FormErrors url.Values `json:"formErrors"`
+	}
+
+	switch r.Method {
+	case "GET", "HEAD":
+		writeResponse := func(w http.ResponseWriter, r *http.Request, response Response) {
+			if r.Form.Has("api") {
+				w.Header().Set("Content-Type", "application/json; charset=utf-8")
+				if r.Method == "HEAD" {
+					w.WriteHeader(http.StatusOK)
+					return
+				}
+				encoder := json.NewEncoder(w)
+				encoder.SetIndent("", "  ")
+				encoder.SetEscapeHTML(false)
+				err := encoder.Encode(&response)
+				if err != nil {
+					getLogger(r.Context()).Error(err.Error())
+				}
+				return
+			}
+			referer := nbrew.getReferer(r)
+			funcMap := map[string]any{
+				"join":       path.Join,
+				"hasPrefix":  strings.HasPrefix,
+				"trimPrefix": strings.TrimPrefix,
+				"contains":   strings.Contains,
+				"stylesCSS":  func() template.CSS { return template.CSS(StylesCSS) },
+				"baselineJS": func() template.JS { return template.JS(BaselineJS) },
+				"referer":    func() string { return referer },
+			}
+			tmpl, err := template.New("changepassword.html").Funcs(funcMap).ParseFS(RuntimeFS, "embed/changepassword.html")
+			if err != nil {
+				getLogger(r.Context()).Error(err.Error())
+				nbrew.internalServerError(w, r, err)
+				return
+			}
+			w.Header().Set("Content-Security-Policy", nbrew.ContentSecurityPolicy)
+			nbrew.executeTemplate(w, r, tmpl, &response)
+		}
+
+		var response Response
+		_, err := nbrew.getSession(r, "flash", &response)
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+		}
+		nbrew.clearSession(w, r, "flash")
+		response.UserID = user.UserID
+		response.Username = user.Username
+		if response.Error != "" {
+			writeResponse(w, r, response)
+			return
+		}
+		writeResponse(w, r, response)
+	case "POST":
+		writeResponse := func(w http.ResponseWriter, r *http.Request, response Response) {
+			if r.Form.Has("api") {
+				w.Header().Set("Content-Type", "application/json; charset=utf-8")
+				if r.Method == "HEAD" {
+					w.WriteHeader(http.StatusOK)
+					return
+				}
+				encoder := json.NewEncoder(w)
+				encoder.SetIndent("", "  ")
+				encoder.SetEscapeHTML(false)
+				err := encoder.Encode(&response)
+				if err != nil {
+					getLogger(r.Context()).Error(err.Error())
+				}
+				return
+			}
+			if response.Error != "" {
+				err := nbrew.setSession(w, r, "flash", &response)
+				if err != nil {
+					getLogger(r.Context()).Error(err.Error())
+					nbrew.internalServerError(w, r, err)
+					return
+				}
+				http.Redirect(w, r, "/users/changepassword/", http.StatusFound)
+				return
+			}
+			err := nbrew.setSession(w, r, "flash", map[string]any{
+				"postRedirectGet": map[string]any{
+					"from": "changepassword",
+				},
+			})
+			if err != nil {
+				getLogger(r.Context()).Error(err.Error())
+				nbrew.internalServerError(w, r, err)
+				return
+			}
+			http.Redirect(w, r, "/users/profile/", http.StatusFound)
+		}
+
+		var request Request
+		r.Body = http.MaxBytesReader(w, r.Body, 1<<20 /* 1 MB */)
+		contentType, _, _ := mime.ParseMediaType(r.Header.Get("Content-Type"))
+		switch contentType {
+		case "application/json":
+			err := json.NewDecoder(r.Body).Decode(&request)
+			if err != nil {
+				nbrew.badRequest(w, r, err)
+				return
+			}
+		case "application/x-www-form-urlencoded", "multipart/form-data":
+			if contentType == "multipart/form-data" {
+				err := r.ParseMultipartForm(1 << 20 /* 1 MB */)
+				if err != nil {
+					nbrew.badRequest(w, r, err)
+					return
+				}
+			} else {
+				err := r.ParseForm()
+				if err != nil {
+					nbrew.badRequest(w, r, err)
+					return
+				}
+			}
+			request.Password = r.Form.Get("password")
+			request.ConfirmPassword = r.Form.Get("confirmPassword")
+		default:
+			nbrew.unsupportedContentType(w, r)
+			return
+		}
+
+		response := Response{
+			FormErrors: url.Values{},
+		}
+		// password
+		if request.Password == "" {
+			response.FormErrors.Add("password", "required")
+		} else {
+			if utf8.RuneCountInString(request.Password) < 8 {
+				response.FormErrors.Add("password", "password must be at least 8 characters")
+			}
+			commonPasswords, err := getCommonPasswords()
+			if err != nil {
+				getLogger(r.Context()).Error(err.Error())
+				nbrew.internalServerError(w, r, err)
+				return
+			}
+			if _, ok := commonPasswords[request.Password]; ok {
+				response.FormErrors.Add("password", "password is too common")
+			}
+		}
+		// confirmPassword
+		if !response.FormErrors.Has("password") {
+			if request.ConfirmPassword == "" {
+				response.FormErrors.Add("confirmPassword", "required")
+			} else {
+				if request.Password != request.ConfirmPassword {
+					response.FormErrors.Add("confirmPassword", "password does not match")
+				}
+			}
+		}
+		if len(response.FormErrors) > 0 {
+			response.Error = "FormErrorsPresent"
+			writeResponse(w, r, response)
+			return
+		}
+		passwordHash, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+			nbrew.internalServerError(w, r, err)
+			return
+		}
+		tx, err := nbrew.DB.Begin()
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+			nbrew.internalServerError(w, r, err)
+			return
+		}
+		defer tx.Rollback()
+		_, err = sq.Exec(r.Context(), tx, sq.Query{
+			Dialect: nbrew.Dialect,
+			Format:  "DELETE FROM authentication WHERE user_id = {userID}",
+			Values: []any{
+				sq.UUIDParam("userID", user.UserID),
+			},
+		})
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+			nbrew.internalServerError(w, r, err)
+			return
+		}
+		_, err = sq.Exec(r.Context(), tx, sq.Query{
+			Dialect: nbrew.Dialect,
+			Format:  "UPDATE users SET password_hash = {passwordHash} WHERE user_id = {userID}",
+			Values: []any{
+				sq.StringParam("passwordHash", string(passwordHash)),
+				sq.UUIDParam("userID", user.UserID),
+			},
+		})
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+			nbrew.internalServerError(w, r, err)
+			return
+		}
+		err = tx.Commit()
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+			nbrew.internalServerError(w, r, err)
+			return
+		}
+		writeResponse(w, r, response)
+	default:
+		nbrew.methodNotAllowed(w, r)
+	}
+}
