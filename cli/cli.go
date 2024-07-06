ddb6c6482b921be68e4d318d74af0f44651cd39d
bokwoon <bokwoon.c@gmail.com>
2024-07-06 15:39:45 +0800

    checkpoint

--- cli/cli.go
+++ cli/cli.go
@@ -45,11 +45,6 @@ type SMTPConfig struct {
 }
 
 func Notebrew(configDir, dataDir string, args []string) (*nb10.Notebrew, error) {
-	// TODO: test if this error on a Windows double click does trigger
-	// pressAnyKeyToExit behaviour.
-	// if true {
-	// 	return nil, fmt.Errorf("foo fighterz!!")
-	// }
 	nbrew := nb10.New()
 	nbrew.Logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true,
