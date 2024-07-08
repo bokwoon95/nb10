64daf0c1e9a31f9a02935ec901f765b81f6feca6
bokwoon <bokwoon.c@gmail.com>
2024-07-08 19:19:35 +0800

    checkpoint

--- cli/createinvite_cmd.go
+++ cli/createinvite_cmd.go
@@ -9,6 +9,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"net/mail"
 	"os"
 	"strconv"
 	"strings"
@@ -22,9 +23,9 @@ import (
 type CreateinviteCmd struct {
 	Notebrew     *nb10.Notebrew
 	Stdout       io.Writer
+	Email        sql.NullString
 	SiteLimit    sql.NullInt64
 	StorageLimit sql.NullInt64
-	Count        sql.NullInt64
 }
 
 func CreateinviteCommand(nbrew *nb10.Notebrew, args ...string) (*CreateinviteCmd, error) {
@@ -34,6 +35,14 @@ func CreateinviteCommand(nbrew *nb10.Notebrew, args ...string) (*CreateinviteCmd
 	var cmd CreateinviteCmd
 	cmd.Notebrew = nbrew
 	flagset := flag.NewFlagSet("", flag.ContinueOnError)
+	flagset.Func("email", "", func(s string) error {
+		_, err := mail.ParseAddress(s)
+		if err != nil {
+			return fmt.Errorf("%q is not a valid email address", s)
+		}
+		cmd.Email = sql.NullString{String: s, Valid: true}
+		return nil
+	})
 	flagset.Func("site-limit", "", func(s string) error {
 		siteLimit, err := strconv.ParseInt(s, 10, 64)
 		if err != nil {
@@ -48,14 +57,6 @@ func CreateinviteCommand(nbrew *nb10.Notebrew, args ...string) (*CreateinviteCmd
 			return fmt.Errorf("%q is not a valid integer", s)
 		}
 		cmd.StorageLimit = sql.NullInt64{Int64: storageLimit, Valid: true}
-		return nil
-	})
-	flagset.Func("count", "", func(s string) error {
-		count, err := strconv.ParseInt(s, 10, 64)
-		if err != nil {
-			return fmt.Errorf("%q is not a valid integer", s)
-		}
-		cmd.Count = sql.NullInt64{Int64: count, Valid: true}
 		return nil
 	})
 	flagset.Usage = func() {
@@ -80,39 +81,34 @@ func (cmd *CreateinviteCmd) Run() error {
 	if cmd.Stdout == nil {
 		cmd.Stdout = os.Stdout
 	}
-	count := 1
-	if cmd.Count.Valid {
-		count = int(cmd.Count.Int64)
-	}
 	scheme := "https://"
 	if !cmd.Notebrew.CMSDomainHTTPS {
 		scheme = "http://"
 	}
-	for i := 0; i < count; i++ {
-		var inviteToken [8 + 16]byte
-		binary.BigEndian.PutUint64(inviteToken[:8], uint64(time.Now().Unix()))
-		_, err := rand.Read(inviteToken[8:])
-		if err != nil {
-			return err
-		}
-		checksum := blake2b.Sum256(inviteToken[8:])
-		var inviteTokenHash [8 + blake2b.Size256]byte
-		copy(inviteTokenHash[:8], inviteToken[:8])
-		copy(inviteTokenHash[8:], checksum[:])
-		_, err = sq.Exec(context.Background(), cmd.Notebrew.DB, sq.Query{
-			Dialect: cmd.Notebrew.Dialect,
-			Format: "INSERT INTO invite (invite_token_hash, site_limit, storage_limit)" +
-				" VALUES ({inviteTokenHash}, {siteLimit}, {storageLimit})",
-			Values: []any{
-				sq.BytesParam("inviteTokenHash", inviteTokenHash[:]),
-				sq.Param("siteLimit", cmd.SiteLimit),
-				sq.Param("storageLimit", cmd.StorageLimit),
-			},
-		})
-		if err != nil {
-			return err
-		}
-		fmt.Fprintln(cmd.Stdout, scheme+cmd.Notebrew.CMSDomain+"/users/invite/?token="+strings.TrimLeft(hex.EncodeToString(inviteToken[:]), "0"))
+	var inviteToken [8 + 16]byte
+	binary.BigEndian.PutUint64(inviteToken[:8], uint64(time.Now().Unix()))
+	_, err := rand.Read(inviteToken[8:])
+	if err != nil {
+		return err
 	}
+	checksum := blake2b.Sum256(inviteToken[8:])
+	var inviteTokenHash [8 + blake2b.Size256]byte
+	copy(inviteTokenHash[:8], inviteToken[:8])
+	copy(inviteTokenHash[8:], checksum[:])
+	_, err = sq.Exec(context.Background(), cmd.Notebrew.DB, sq.Query{
+		Dialect: cmd.Notebrew.Dialect,
+		Format: "INSERT INTO invite (invite_token_hash, email, site_limit, storage_limit)" +
+			" VALUES ({inviteTokenHash}, {email}, {siteLimit}, {storageLimit})",
+		Values: []any{
+			sq.BytesParam("inviteTokenHash", inviteTokenHash[:]),
+			sq.Param("email", cmd.Email),
+			sq.Param("siteLimit", cmd.SiteLimit),
+			sq.Param("storageLimit", cmd.StorageLimit),
+		},
+	})
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(cmd.Stdout, scheme+cmd.Notebrew.CMSDomain+"/users/invite/?token="+strings.TrimLeft(hex.EncodeToString(inviteToken[:]), "0"))
 	return nil
 }
