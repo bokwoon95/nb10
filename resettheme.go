046c09ef61c9fbf3d34f4f19b81dfe6ea1534894
bokwoon <bokwoon.c@gmail.com>
2024-07-08 22:52:48 +0800

    checkpoint

--- resettheme.go
+++ resettheme.go
@@ -1,19 +1,93 @@
 package nb10
 
-import "net/http"
+import (
+	"encoding/json"
+	"html/template"
+	"net/http"
+	"net/url"
+	"path"
+	"strings"
+)
 
 func (nbrew *Notebrew) resettheme(w http.ResponseWriter, r *http.Request, user User, sitePrefix string) {
 	type Request struct {
-		ResetAllCategories bool
-		Category           string
+		ResetIndexHTML                 bool   `json:"resetIndexHTML"`
+		ResetPostHTML                  bool   `json:"resetPostHTML"`
+		ResetPostHTMLAllCategories     bool   `json:"resetPostHTMLAllCategories"`
+		ResetPostHTMLCategory          string `json:"resetPostHTMLCategory"`
+		ResetPostListHTML              bool   `json:"resetPostListHTML"`
+		ResetPostListHTMLAllCategories bool   `json:"resetPostListHTMLAllCategories"`
+		ResetPostListHTMLCategory      string `json:"resetPostListHTMLCategory"`
 	}
 	type Response struct {
-		ContentBaseURL string   `json:"contentBaseURL"`
-		IsDatabaseFS   bool     `json:"isDatabaseFS"`
-		SitePrefix     string   `json:"sitePrefix"`
-		UserID         ID       `json:"userID"`
-		Username       string   `json:"username"`
-		DisableReason  string   `json:"disableReason"`
-		Categories     []string `json:"categories"`
+		ContentBaseURL    string            `json:"contentBaseURL"`
+		IsDatabaseFS      bool              `json:"isDatabaseFS"`
+		SitePrefix        string            `json:"sitePrefix"`
+		UserID            ID                `json:"userID"`
+		Username          string            `json:"username"`
+		DisableReason     string            `json:"disableReason"`
+		Categories        []string          `json:"categories"`
+		Error             string            `json:"error"`
+		FormErrors        url.Values        `json:"formErrors"`
+		RegenerationStats RegenerationStats `json:"regenerationStats"`
+	}
+
+	switch r.Method {
+	case "GET", "HEAD":
+		writeResponse := func(w http.ResponseWriter, r *http.Request, response Response) {
+			if r.Form.Has("api") {
+				w.Header().Set("Content-Type", "application/json; charset=utf-8")
+				if r.Method == "HEAD" {
+					w.WriteHeader(http.StatusOK)
+					return
+				}
+				encoder := json.NewEncoder(w)
+				encoder.SetIndent("", "  ")
+				encoder.SetEscapeHTML(false)
+				err := encoder.Encode(&response)
+				if err != nil {
+					getLogger(r.Context()).Error(err.Error())
+				}
+				return
+			}
+			referer := nbrew.GetReferer(r)
+			funcMap := map[string]any{
+				"join":       path.Join,
+				"base":       path.Base,
+				"hasPrefix":  strings.HasPrefix,
+				"trimPrefix": strings.TrimPrefix,
+				"stylesCSS":  func() template.CSS { return template.CSS(StylesCSS) },
+				"baselineJS": func() template.JS { return template.JS(BaselineJS) },
+				"referer":    func() string { return referer },
+			}
+			tmpl, err := template.New("resettheme.html").Funcs(funcMap).ParseFS(RuntimeFS, "embed/resettheme.html")
+			if err != nil {
+				getLogger(r.Context()).Error(err.Error())
+				nbrew.InternalServerError(w, r, err)
+				return
+			}
+			w.Header().Set("Content-Security-Policy", nbrew.ContentSecurityPolicy)
+			nbrew.ExecuteTemplate(w, r, tmpl, &response)
+		}
+
+		var response Response
+		_, err := nbrew.GetSession(r, "flash", &response)
+		if err != nil {
+			getLogger(r.Context()).Error(err.Error())
+		}
+		nbrew.ClearSession(w, r, "flash")
+		response.ContentBaseURL = nbrew.ContentBaseURL(sitePrefix)
+		response.SitePrefix = sitePrefix
+		response.UserID = user.UserID
+		response.Username = user.Username
+		response.DisableReason = user.DisableReason
+		if response.Error != "" {
+			writeResponse(w, r, response)
+			return
+		}
+		writeResponse(w, r, response)
+	case "POST":
+	default:
+		nbrew.MethodNotAllowed(w, r)
 	}
 }
