f93c4e7c9106897692bc0742060b0d7c22d08bc5
bokwoon <bokwoon.c@gmail.com>
2024-08-01 20:13:10 +0800

    checkpoint

--- fs_dir.go
+++ fs_dir.go
@@ -58,19 +58,6 @@ func NewDirFS(config DirFSConfig) (*DirFS, error) {
 	return dirFS, nil
 }
 
-func (fsys *DirFS) As(target any) bool {
-	switch target := target.(type) {
-	case *DirFS:
-		*target = *fsys
-		return true
-	case **DirFS:
-		*target = fsys
-		return true
-	default:
-		return false
-	}
-}
-
 func (fsys *DirFS) WithContext(ctx context.Context) FS {
 	return &DirFS{
 		RootDir: fsys.RootDir,
