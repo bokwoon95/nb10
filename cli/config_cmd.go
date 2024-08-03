2546e3f03ec13bdbf956fb79db1ff58cd731aa38
bokwoon <bokwoon.c@gmail.com>
2024-08-03 16:06:58 +0800

    checkpoint

--- cli/config_cmd.go
+++ cli/config_cmd.go
@@ -959,7 +959,7 @@ const filesHelp = `# == files keys == #
 # Refer to ` + "`notebrew config`" + ` on how to get and set config values.
 # provider - Files provider (possible values: directory, database, sftp)
 # dialect  - Database dialect (possible values: sqlite, postgres, mysql).
-# filePath - Files root directory (if using a directory) or file path to the sqlite file (if using sqlite).
+# filePath - Files root directory (if using a directory), file path to the sqlite file (if using sqlite database) or remote root directory (if using SFTP).
 # user     - Database/SFTP user
 # password - Database/SFTP password
 # host     - Database/SFTP host
