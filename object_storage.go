8c8ef208eeaef27d503ddf32ea07e2f4c5ef083a
bokwoon <bokwoon.c@gmail.com>
2024-08-17 21:39:50 +0800

    checkpoint

--- object_storage.go
+++ object_storage.go
@@ -4,14 +4,12 @@ import (
 	"bytes"
 	"context"
 	"errors"
-	"fmt"
 	"io"
 	"io/fs"
 	"log/slog"
 	"os"
 	"path"
 	"path/filepath"
-	"sync"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/credentials"
@@ -21,30 +19,68 @@ import (
 	"github.com/bokwoon95/nb10/stacktrace"
 )
 
+// ObjectStorage represents an object storage provider.
 type ObjectStorage interface {
+	// Gets an object from a bucket.
 	Get(ctx context.Context, key string) (io.ReadCloser, error)
+
+	// Puts an object into a bucket. If key already exists, it should be
+	// replaced.
 	Put(ctx context.Context, key string, reader io.Reader) error
+
+	// Deletes an object from a bucket. It returns no error if the object does
+	// not exist.
 	Delete(ctx context.Context, key string) error
+
+	// Copies an object identified by srcKey into destKey. srcKey should exist.
+	// If destKey already exists, it should be replaced.
 	Copy(ctx context.Context, srcKey, destKey string) error
 }
 
+// S3ObjectStorage implements ObjectStorage via an S3-compatible provider.
 type S3ObjectStorage struct {
+	// S3 SDK client.
 	Client *s3.Client
+
+	// S3 Bucket to put objects in.
 	Bucket string
+
+	// File extension to Content-Type map.
+	ContentTypeMap map[string]string
+
+	// Logger is used for reporting errors that cannot be handled and are
+	// thrown away.
 	Logger *slog.Logger
 }
 
 var _ ObjectStorage = (*S3ObjectStorage)(nil)
 
+// S3StorageConfig holds the parameters needed to construct an S3ObjectStorage.
 type S3StorageConfig struct {
-	Endpoint        string
-	Region          string
-	Bucket          string
-	AccessKeyID     string
+	// (Required) S3 endpoint.
+	Endpoint string
+
+	// (Required) S3 region.
+	Region string
+
+	// (Required) S3 bucket.
+	Bucket string
+
+	// (Required) S3 access key ID.
+	AccessKeyID string
+
+	// (Required) S3 secret access key.
 	SecretAccessKey string
-	Logger          *slog.Logger
+
+	// File extension to Content-Type map.
+	ContentTypeMap map[string]string
+
+	// (Required) Logger is used for reporting errors that cannot be handled
+	// and are thrown away.
+	Logger *slog.Logger
 }
 
+// NewS3Storage constructs a new S3ObjectStorage.
 func NewS3Storage(ctx context.Context, config S3StorageConfig) (*S3ObjectStorage, error) {
 	storage := &S3ObjectStorage{
 		Client: s3.New(s3.Options{
@@ -52,8 +88,9 @@ func NewS3Storage(ctx context.Context, config S3StorageConfig) (*S3ObjectStorage
 			Region:       config.Region,
 			Credentials:  aws.NewCredentialsCache(credentials.NewStaticCredentialsProvider(config.AccessKeyID, config.SecretAccessKey, "")),
 		}),
-		Bucket: config.Bucket,
-		Logger: config.Logger,
+		Bucket:         config.Bucket,
+		ContentTypeMap: config.ContentTypeMap,
+		Logger:         config.Logger,
 	}
 	// Ping the bucket and see if we have access.
 	_, err := storage.Client.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
@@ -66,6 +103,7 @@ func NewS3Storage(ctx context.Context, config S3StorageConfig) (*S3ObjectStorage
 	return storage, nil
 }
 
+// Get implements the Get ObjectStorage operation for S3ObjectStorage.
 func (storage *S3ObjectStorage) Get(ctx context.Context, key string) (io.ReadCloser, error) {
 	output, err := storage.Client.GetObject(ctx, &s3.GetObjectInput{
 		Bucket: &storage.Bucket,
@@ -83,11 +121,8 @@ func (storage *S3ObjectStorage) Get(ctx context.Context, key string) (io.ReadClo
 	return output.Body, nil
 }
 
+// Put implements the Put ObjectStorage operation for S3ObjectStorage.
 func (storage *S3ObjectStorage) Put(ctx context.Context, key string, reader io.Reader) error {
-	fileType, ok := AllowedFileTypes[path.Ext(key)]
-	if !ok || !fileType.Has(AttributeObject) {
-		return fmt.Errorf("%s: invalid filetype %s", key, path.Ext(key))
-	}
 	cleanup := func(uploadId *string) {
 		_, err := storage.Client.AbortMultipartUpload(context.Background(), &s3.AbortMultipartUploadInput{
 			Bucket:   &storage.Bucket,
@@ -98,11 +133,15 @@ func (storage *S3ObjectStorage) Put(ctx context.Context, key string, reader io.R
 			storage.Logger.Error(stacktrace.New(err).Error())
 		}
 	}
+	contentType := storage.ContentTypeMap[path.Ext(key)]
+	if contentType == "" {
+		contentType = "application/octet-stream"
+	}
 	createResult, err := storage.Client.CreateMultipartUpload(ctx, &s3.CreateMultipartUploadInput{
 		Bucket:       &storage.Bucket,
 		Key:          aws.String(key),
 		CacheControl: aws.String("max-age=31536000, immutable" /* 1 year */),
-		ContentType:  aws.String(fileType.ContentType),
+		ContentType:  aws.String(contentType),
 	})
 	if err != nil {
 		return stacktrace.New(err)
@@ -154,6 +193,7 @@ func (storage *S3ObjectStorage) Put(ctx context.Context, key string, reader io.R
 	return nil
 }
 
+// Delete implements the Delete ObjectStorage operation for S3ObjectStorage.
 func (storage *S3ObjectStorage) Delete(ctx context.Context, key string) error {
 	_, err := storage.Client.DeleteObject(ctx, &s3.DeleteObjectInput{
 		Bucket: &storage.Bucket,
@@ -165,6 +205,7 @@ func (storage *S3ObjectStorage) Delete(ctx context.Context, key string) error {
 	return nil
 }
 
+// Copy implements the Copy ObjectStorage operation for S3ObjectStorage.
 func (storage *S3ObjectStorage) Copy(ctx context.Context, srcKey, destKey string) error {
 	_, err := storage.Client.CopyObject(ctx, &s3.CopyObjectInput{
 		Bucket:     &storage.Bucket,
@@ -183,28 +224,26 @@ func (storage *S3ObjectStorage) Copy(ctx context.Context, srcKey, destKey string
 	return nil
 }
 
+// DirectoryObjectStorage implements ObjectStorage using a local directory.
 type DirectoryObjectStorage struct {
+	// Root directory to store objects in.
 	RootDir string
-	TempDir string
 }
 
+// NewDirObjectStorage constructs a new DirectoryObjectStorage.
 func NewDirObjectStorage(rootDir, tempDir string) (*DirectoryObjectStorage, error) {
 	var err error
 	rootDir, err = filepath.Abs(filepath.FromSlash(rootDir))
 	if err != nil {
 		return nil, err
 	}
-	tempDir, err = filepath.Abs(filepath.FromSlash(tempDir))
-	if err != nil {
-		return nil, err
-	}
 	directoryObjectStorage := &DirectoryObjectStorage{
 		RootDir: filepath.FromSlash(rootDir),
-		TempDir: filepath.FromSlash(tempDir),
 	}
 	return directoryObjectStorage, nil
 }
 
+// Get implements the Get ObjectStorage operation for DirectoryObjectStorage.
 func (storage *DirectoryObjectStorage) Get(ctx context.Context, key string) (io.ReadCloser, error) {
 	err := ctx.Err()
 	if err != nil {
@@ -223,6 +262,7 @@ func (storage *DirectoryObjectStorage) Get(ctx context.Context, key string) (io.
 	return file, nil
 }
 
+// Put implements the Put ObjectStorage operation for DirectoryObjectStorage.
 func (storage *DirectoryObjectStorage) Put(ctx context.Context, key string, reader io.Reader) error {
 	err := ctx.Err()
 	if err != nil {
@@ -252,6 +292,8 @@ func (storage *DirectoryObjectStorage) Put(ctx context.Context, key string, read
 	return nil
 }
 
+// Delete implements the Delete ObjectStorage operation for
+// DirectoryObjectStorage.
 func (storage *DirectoryObjectStorage) Delete(ctx context.Context, key string) error {
 	err := ctx.Err()
 	if err != nil {
@@ -270,6 +312,7 @@ func (storage *DirectoryObjectStorage) Delete(ctx context.Context, key string) e
 	return nil
 }
 
+// Copy implements the Copy ObjectStorage operation for DirectoryObjectStorage.
 func (storage *DirectoryObjectStorage) Copy(ctx context.Context, srcKey, destKey string) error {
 	err := ctx.Err()
 	if err != nil {
@@ -302,58 +345,5 @@ func (storage *DirectoryObjectStorage) Copy(ctx context.Context, srcKey, destKey
 	if err != nil {
 		return stacktrace.New(err)
 	}
-	return nil
-}
-
-type InMemoryObjectStorage struct {
-	mu      sync.RWMutex
-	entries map[string][]byte
-}
-
-var _ ObjectStorage = (*InMemoryObjectStorage)(nil)
-
-func NewInMemoryObjectStorage() *InMemoryObjectStorage {
-	return &InMemoryObjectStorage{
-		mu:      sync.RWMutex{},
-		entries: make(map[string][]byte),
-	}
-}
-
-func (storage *InMemoryObjectStorage) Get(ctx context.Context, key string) (io.ReadCloser, error) {
-	storage.mu.RLock()
-	value, ok := storage.entries[key]
-	storage.mu.RUnlock()
-	if !ok {
-		return nil, &fs.PathError{Op: "get", Path: key, Err: fs.ErrNotExist}
-	}
-	return io.NopCloser(bytes.NewReader(value)), nil
-}
-
-func (storage *InMemoryObjectStorage) Put(ctx context.Context, key string, reader io.Reader) error {
-	value, err := io.ReadAll(reader)
-	if err != nil {
-		return err
-	}
-	storage.mu.Lock()
-	storage.entries[key] = value
-	storage.mu.Unlock()
-	return nil
-}
-
-func (storage *InMemoryObjectStorage) Delete(ctx context.Context, key string) error {
-	storage.mu.Lock()
-	delete(storage.entries, key)
-	storage.mu.Unlock()
-	return nil
-}
-
-func (storage *InMemoryObjectStorage) Copy(ctx context.Context, srcKey, destKey string) error {
-	storage.mu.Lock()
-	value, ok := storage.entries[srcKey]
-	if !ok {
-		return &fs.PathError{Op: "copy", Path: srcKey, Err: fs.ErrNotExist}
-	}
-	storage.entries[destKey] = value
-	storage.mu.Unlock()
 	return nil
 }
